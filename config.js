fd6aca9bf023355b573e2e142cbe0a60d76e6d5e
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2023-11-26 21:35:57 +0000

    add support for dotenv and sample.

---
+++ config.js
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+// Support for replit secrets
+if(process.env.NODE_ENV === 'replit')
+{
+  dotenv.config();
+  process.env.HOST = `${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
+}
+else
+{
+  dotenv.config({
+      path: path.resolve(__dirname, `env/${process.env.NODE_ENV}.env`)
+  });
+}
+
+//update host for replit
+
+const config = {
+  NODE_ENV: process.env.NODE_ENV || 'dev',
+  HOST: process.env.HOST || 'localhost',
+  PORT: process.env.PORT || 3000,
+  clientId: process.env.clientId || 'clientId',
+  clientSecret: process.env.clientSecret || 'clientSecret',
+  idpUri: process.env.idpUri || 'idpUri',
+  sessionSecret: process.env.sessionSecret || 'sessionSecret',
+  tenantUri: process.env.tenantUri || 'tenantUri',
+  redirectPage: process.env.redirectPage || 'redirectPage',
+}
+
+export { config };
